f1f14bb73e63721434548425d032435753f6a0c0
MacBook <ramovitabella@gmail.com>
2019-05-08 15:01:55 +0900

    h2 jpa add

--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -3,26 +3,33 @@ package net.slipp.web;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import net.slipp.domain.User;
+import net.slipp.domain.UserRepository;
+
 @Controller
 public class UserController {
 	private List<User> users = new ArrayList<User>();
 	
+	@Autowired
+	private UserRepository userRepository;
+	
 	@PostMapping("/create")
 	public String create(User user) {
 		System.out.println(user.toString());
-		users.add(user);
+		userRepository.save(user);
 		
 		return "redirect:/list";
 	}
 	
 	@GetMapping("/list")
 	public String list(Model model) {
-		model.addAttribute("users", users);
+		model.addAttribute("users", userRepository.findAll());
 		
 		return "list";
 	}
