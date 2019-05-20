3d93adc9144e0c6d31107093bfa5992f1d415f73
MacBook <ramovitabella@gmail.com>
2019-05-20 15:43:09 +0900

    질문 상세보기 기능 추가

--- src/main/java/net/slipp/web/QuestionController.java
+++ src/main/java/net/slipp/web/QuestionController.java
@@ -4,7 +4,9 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -41,5 +43,12 @@ public class QuestionController {
 		questionRepository.save(newQuestion);
 		
 		return "redirect:/";
+	}
+	
+	@GetMapping("/{id}")
+	public String show(@PathVariable Long id, Model model) {
+		Question question = questionRepository.findById(id).get();
+		model.addAttribute("question", question);
+		return "/qna/show";
 	}
 }
