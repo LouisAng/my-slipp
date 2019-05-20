2ee39aeae95ac547e0c9c21ab4170c4f64ed1067
MacBook <ramovitabella@gmail.com>
2019-05-20 16:31:22 +0900

    question  사용자 확인, 객체 equals 범위 수정 추가, 샘플데이터 추가

--- src/main/java/net/slipp/domain/Question.java
+++ src/main/java/net/slipp/domain/Question.java
@@ -44,4 +44,35 @@ public class Question {
 		this.contents = contents;
 		
 	}
+
+	public boolean isSameWriter(User loginUser) {
+		return this.writer.equals(loginUser);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Question other = (Question) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
 }
